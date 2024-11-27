01a2ad0fe7635528ea5796b58b470ba94d05e63c
kamila.olszowa <kamila.olszowa@hotmail.com>
2024-11-27 18:39:29 +0100

    initial commit

---
+++ src/app/components/pokemons-details/pokemons-details.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pokemons-details',
+  imports: [],
+  templateUrl: './pokemons-details.component.html',
+  styleUrl: './pokemons-details.component.scss'
+})
+export class PokemonsDetailsComponent {
+
+}
---
+++ src/app/components/pokemons-list/pokemons-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pokemons-list',
+  imports: [],
+  templateUrl: './pokemons-list.component.html',
+  styleUrl: './pokemons-list.component.scss'
+})
+export class PokemonsListComponent {
+
+}
